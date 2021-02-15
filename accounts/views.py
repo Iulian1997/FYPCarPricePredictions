9857e3e027d639ac81377d62915e58aeaed06cf4
Iulian Gherman <Iulian.Gherman@mycit.ie>
2021-02-15 01:48:45 +0000

    Dynamic Page Titles

--- accounts/views.py
+++ accounts/views.py
@@ -62,7 +62,11 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    # Logout
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, ': You are now logged out!')
+        return redirect('index')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
+// Error message fade out after 3 seconds
 setTimeout(function() {
-    $('message').fadeOut('slow');
+    $('#message').fadeOut('slow');
 }, 3000);
