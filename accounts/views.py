c38d5c1fffde928d68fd2651eaf05015428e52bb
Iulian Gherman <Iulian.Gherman@mycit.ie>
2021-02-15 01:11:11 +0000

    Login Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -43,8 +43,21 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login User
-        return
+        # Get Form Values
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+
+        # Login Validation
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, ': You are now logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Wrong Credentials!')
+            return redirect('login')
+
     else:
         return render(request, 'accounts/login.html')
 
